dab08f4528eae52f04c3a385f21e5ff63700f2b1
Aidil-Nov <lomantp537.com>
2025-03-29 11:00:31 +0700

    add alpine js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,5 @@
-import './bootstrap';
-const feather = require('feather-icons');
+import "./bootstrap";
+const feather = require("feather-icons");
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start();
