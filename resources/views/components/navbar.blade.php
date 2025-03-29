7fb6fbb76353fdad9e07857464db0de2eea95f3e
Aidil-Nov <lomantp537.com>
2025-03-29 11:00:52 +0700

    add dropdown

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,19 +14,44 @@
         </div>
 
         <!-- Keuangan -->
-        <div class="space-y-1 px-4 rounded mb-2">
-            <a href="{{ route('keuangan') }}"
-                class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors
+        <div x-data="{dropdownOpen:false}" class="space-y-1 px-4 rounded mb-2 dropdown">
+            <!-- induk -->
+            <a @click="dropdownOpen = !dropdownOpen" 
+                class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors justify-between
                 {{ request()->routeIs('keuangan') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
-                <i data-feather="dollar-sign"></i>
-                <span>Keuangan</span>
+                <div class="flex space-x-3">
+                    <i data-feather="dollar-sign"></i>
+                    <span>Keuangan</span>
+                </div>
+                <i data-feather="chevron-down" class="justify-end hidden"></i>
             </a>
+            <!-- anak -->
+            <div class="ml-7 pl-3 border-l-gray-50 border-l-2" x-show="dropdownOpen" x-transition:enter.duration.500ms
+            x-transition:leave.duration.400ms>
+                <!-- itemnya -->
+                <a href="{{ route('keuangan') }}" class="flex items-center space-x-3 px-6 py-1 rounded-lg transition-colors
+                {{ request()->routeIs('laporan') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
+                    <span>Laporan</span>
+                </a>
+                <a href="{{ route('keuangan') }}" class="flex items-center space-x-3 px-6 py-1 rounded-lg transition-colors
+                {{ request()->routeIs('laporan') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
+                    <span>Laporan</span>
+                </a>
+                <a href="{{ route('keuangan') }}" class="flex items-center space-x-3 px-6 py-1 rounded-lg transition-colors
+                {{ request()->routeIs('laporan') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
+                    <span>Laporan</span>
+                </a>
+                <a href="{{ route('keuangan') }}" class="flex items-center space-x-3 px-6 py-1 rounded-lg transition-colors
+                {{ request()->routeIs('laporan') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
+                    <span>Laporan</span>
+                </a>
+
+            </div>
         </div>
 
         <!-- Absensi -->
         <div class="space-y-1 px-4 rounded mb-2">
-            <a href="{{ route('absensi') }}"
-                class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors
+            <a href="{{ route('absensi') }}" class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors
                 {{ request()->routeIs('absensi') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
                 <i data-feather="user-check"></i>
                 <span>Absensi</span>
@@ -45,8 +70,7 @@
 
         <!-- Stok -->
         <div class="space-y-1 px-4 rounded mb-2">
-            <a href="{{ route('stok') }}"
-                class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors
+            <a href="{{ route('stok') }}" class="flex items-center space-x-3 px-4 py-3 rounded-lg transition-colors
                 {{ request()->routeIs('stok') ? 'bg-gray-900 text-gray-100' : 'text-gray-400 hover:bg-gray-700' }}">
                 <i data-feather="layers"></i>
                 <span>Stok Material</span>
@@ -57,18 +81,20 @@
 
 @push('scripts')
     <script src="https://unpkg.com/feather-icons"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             feather.replace();
-            
+
             // Untuk menu dropdown (jika ada)
             document.querySelectorAll('.arrow_down').forEach(chevron => {
-                chevron.addEventListener('click', function() {
+                chevron.addEventListener('click', function () {
                     const submenu = this.closest('li').querySelector('.submenu');
                     submenu.classList.toggle('hidden');
                     feather.replace();
                 });
             });
         });
+
     </script>
 @endpush
